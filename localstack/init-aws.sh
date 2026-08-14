b739ec10524ee30fd294ae3588220bd0973695b6
Luishbarros <luishrbr@outlook.com>
2026-08-14 07:05:01 -0300

    fix(infra): externalize DB credentials to .env and .gitignore (I8)
    - Move real-looking DB passwords from application.yml and init-aws.sh into a root `.env` file.
    - Add `.env` to `.gitignore`.
    - Configure `spring.config.import` in auth, chat, game, and social application.yml to load the `.env` file.
    - Add `env_file: - ../.env` to auth, game, and social docker-compose for LocalStack init.
    - Update localstack/init-aws.sh to use `${*_DB_PASSWORD}` with local defaults.
    
    Relates to inconsistencies I8.

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -81,7 +81,7 @@ awslocal rds create-db-instance \
   --db-name auth_db \
   --engine postgres \
   --master-username auth_user \
-  --master-user-password auth_pass \
+  --master-user-password ${AUTH_DB_PASSWORD:-auth_pass} \
   --allocated-storage 20 || true
 
 awslocal rds create-db-instance \
@@ -89,7 +89,7 @@ awslocal rds create-db-instance \
   --db-name social_db \
   --engine postgres \
   --master-username social_user \
-  --master-user-password social_pass \
+  --master-user-password ${SOCIAL_DB_PASSWORD:-social_pass} \
   --allocated-storage 20 || true
 
 awslocal rds create-db-instance \
@@ -97,7 +97,7 @@ awslocal rds create-db-instance \
   --db-name chat_db \
   --engine postgres \
   --master-username chat_user \
-  --master-user-password chat_pass \
+  --master-user-password ${CHAT_DB_PASSWORD:-chat_pass} \
   --allocated-storage 20 || true
 
 awslocal rds create-db-instance \
@@ -105,7 +105,7 @@ awslocal rds create-db-instance \
   --db-name game_db \
   --engine postgres \
   --master-username game_user \
-  --master-user-password game_pass \
+  --master-user-password ${GAME_DB_PASSWORD:-game_pass} \
   --allocated-storage 20 || true
 
 # 7. Create DynamoDB Tables
